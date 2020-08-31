b9cadea9291ce3728ab0f22c64a98c9d0bdd4640
Rokon <rokon@itebd.com>
2020-08-31 14:31:36 +0600

    sdfasdf

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! I have created a NetCore project!");
+        }
+    }
+}
